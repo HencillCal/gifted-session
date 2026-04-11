4e81d252a28b67732f31e52f8c32bf9652f1473b
Maurice Gift <maurice@giftedtech.co.ke>
2026-04-11 10:19:13 +0000

    fix: remove hardcoded MongoDB URI from config, update ecosystem cwd, WA channel confirmed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,9 @@ module.exports = {
   apps: [{
     name: "atassasess",
     script: "index.js",
-    cwd: "/home/gifted/.web/gifted-session",
+    cwd: "/root/web/gifted-session",
     env: {
-      PORT: 50900,
-      DATABASE_URL: "mongodb+srv://giftedke:8dbJUy1inRFQfz4x@atassasess.m4t7wun.mongodb.net/?appName=atassasess"
+      PORT: 6898
     }
   }]
 };
