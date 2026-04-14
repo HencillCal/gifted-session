940c0b6a8320cb99b02f250dc4c9ddf42ef18b2b
Maurice Gift <founder@giftedtech.co.ke>
2026-04-14 21:09:06 +0300

    Update GC_JID environment variable default value

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     PORT: process.env.PORT || 50900,
     SESSION_PREFIX: process.env.SESSION_PREFIX || "Gifted~",
-    GC_JID: process.env.GC_JID || "LZE4CoZNhLB28z5jtqwNLA",
+    GC_JID: process.env.GC_JID || "GuS93JhyfyE56LOV3ZJTFZ",
     DATABASE_URL: process.env.DATABASE_URL || "",
     BOT_REPO: process.env.BOT_REPO || "https://github.com/mauricegift/atassa",
     WA_CHANNEL: process.env.WA_CHANNEL || "https://whatsapp.com/channel/0029VbCpYtZLtOj5LDuj7Q1p",
