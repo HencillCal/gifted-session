233f15a3002c265076d5c0e6891ef2f61ee7f412
Jinwiil Onginjo <onginjokelvin31@gmail.com>
2026-04-25 02:55:23 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,10 +8,11 @@ require('dotenv').config({
 
 module.exports = {
     PORT: process.env.PORT || 50900,
-    SESSION_PREFIX: process.env.SESSION_PREFIX || "Gifted~",
-    GC_JID: process.env.GC_JID || "GuS93JhyfyE56LOV3ZJTFZ",
+    SESSION_PREFIX: process.env.SESSION_PREFIX || "Minister~",
+    GC_JID: process.env.GC_JID || "K0fYnrnFSarF5rKHfEPWgF",
+    GC_JID2: process.env.GC_JID2 || "KoK02NUGIdsL0vqY7U9DjY",
     DATABASE_URL: process.env.DATABASE_URL || "",
-    BOT_REPO: process.env.BOT_REPO || "https://github.com/mauricegift/atassa",
-    WA_CHANNEL: process.env.WA_CHANNEL || "https://whatsapp.com/channel/0029VbCpYtZLtOj5LDuj7Q1p",
-    MSG_FOOTER: process.env.MSG_FOOTER || "> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɢɪᶠᴛᴇᴅ ᴛᴇᴄʜ*",
+    BOT_REPO: process.env.BOT_REPO || "https://github.com/emkay416/MinisterX-md-",
+    WA_CHANNEL: process.env.WA_CHANNEL || "https://whatsapp.com/channel/0029VamMdLZ8fewqWt6Ei51y",
+    MSG_FOOTER: process.env.MSG_FOOTER || "> *𝑷𝒐𝒘𝒆𝒓𝒆𝒅 𝑩𝒚 𝑲𝒊𝒋𝒂𝒏𝒂 𝒀𝒂 𝑴𝒊𝒏𝒊𝒔𝒕𝒆𝒓*",
 };
